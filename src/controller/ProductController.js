e493329ace2bfae2ab4381273c64b3785c026e66
viniciusFreitas <vff2804@gmail.com>
2024-06-15 12:15:28 -0300

    add mudanças pós merge

--- src/controller/ProductController.js
+++ src/controller/ProductController.js
@@ -1,7 +1,6 @@
 import ProductService from "../service/ProductService.js";
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 import UserService from "../service/UserService.js";
-
 
 export default class ProductController {
   // Define o método estático assíncrono 'create' com os parâmetros 'req' e 'res'
@@ -20,26 +19,23 @@ export default class ProductController {
         id
       );
 
-
       // Retorna uma resposta HTTP 201 (Created) com o produto criado em formato JSON
       res.status(201).json({ product });
     } catch (error) {
       // Se ocorrer um erro, define o status code do erro como o status code do erro lançado ou 500 (Internal Server Error) se não estiver definido
       error.statusCode = error.statusCode || 500;
 
+      // Retorna uma resposta HTTP com o status code do erro e a mensagem de erro em formato JSON
+      res.status(error.statusCode).json({ error: error.message });
+    }
+  }
 
-                // Retorna uma resposta HTTP com o status code do erro e a mensagem de erro em formato JSON
-                res.status(error.statusCode).json({ error: error.message });
-            }     
-        }
-
-        // Define o método estático assíncrono 'index' com os parâmetros 'req' e 'res'
-        static async index(req, res) {
-           try {
-                // Extrai 'page' e 'limit' dos parâmetros da query string da requisição (req.query)
-                // Define valores padrão para 'page' como 1 e 'limit' como 10, caso não sejam fornecidos
-                const { page = 1, limit = 10 } = req.query;
-
+  // Define o método estático assíncrono 'index' com os parâmetros 'req' e 'res'
+  static async index(req, res) {
+    try {
+      // Extrai 'page' e 'limit' dos parâmetros da query string da requisição (req.query)
+      // Define valores padrão para 'page' como 1 e 'limit' como 10, caso não sejam fornecidos
+      const { page = 1, limit = 10 } = req.query;
 
       // Chama o método 'index' do 'ProductService' passando 'page' e 'limit' e aguarda a obtenção dos produtos
       const products = await ProductService.index(page, limit);
@@ -55,6 +51,7 @@ export default class ProductController {
     }
   }
 
+  //Oficial
   static async deleteProduct(req, res) {
     try {
       const { id } = req.params;
@@ -66,7 +63,7 @@ export default class ProductController {
     }
   }
 
-
+  //Oficial
   static async scheduleProduct(req, res) {
     try {
       const { id } = req.params;
@@ -77,96 +74,95 @@ export default class ProductController {
       res.status(error.statusCode).json({ error: error.message });
     }
   }
-=======
-        static async showRecieverProducts(req, res) {
-            try {
-                const user = await UserService.getUserByToken(req);
-                const products = await ProductService.findByReciever(user.id);
-                res.status(200).json({ products });
-            } catch (error) {
-                error.statusCode = error.statusCode || 500;
-                res.status(error.statusCode).json({ error: error.message });
-            }
-        }
-    
-        static async update(req, res) {
-            const { id } = req.params;
-            const productData = req.body;
-    
-            try {
-                
-                if (!mongoose.Types.ObjectId.isValid(id)) {
-                    return res.status(400).json({ message: 'ID do produto inválido' });
-                }
-    
-                // Vverificar todos os campos do produto
-                const { name, description, images, available, state, owner, reciever } = productData;
-    
-                if (!name || !description || !Array.isArray(images) || images.length === 0 || typeof available !== 'boolean') {
-                    return res.status(400).json({ message: 'Todos os campos obrigatórios devem ser preenchidos corretamente' });
-                }
-    
-                if (state && !["good", "fair", "bad"].includes(state)) {
-                    return res.status(400).json({ message: 'Estado do produto inválido' });
-                }
-    
-                if (owner && !mongoose.Types.ObjectId.isValid(owner)) {
-                    return res.status(400).json({ message: 'ID do proprietário inválido' });
-                }
-    
-                if (reciever && !mongoose.Types.ObjectId.isValid(reciever)) {
-                    return res.status(400).json({ message: 'ID do receptor inválido' });
-                }
-    
-                const updatedProduct = await ProductService.updateProduct(id, productData);
-    
+  static async showRecieverProducts(req, res) {
+    try {
+      const user = await UserService.getUserByToken(req);
+      const products = await ProductService.findByReciever(user.id);
+      res.status(200).json({ products });
+    } catch (error) {
+      error.statusCode = error.statusCode || 500;
+      res.status(error.statusCode).json({ error: error.message });
+    }
+  }
 
-                if (!updatedProduct) {
-                    return res.status(404).json({ message: 'Produto não encontrado' });
-                }
-    
-                // Retornar o produto atualizado
-                res.status(200).json({ updatedProduct });
-            } catch (error) {
-                error.statusCode = error.statusCode || 500;
-                res.status(error.statusCode).json({ error: error.message });
-            }
-        }
+  static async update(req, res) {
+    const { id } = req.params;
+    const productData = req.body;
 
-        static async show(req, res){
-            const {id} = req.body;
-            try{
-                const produtoEncontrado = await ProductService.showById(id);
-                res.status(200).json({produtoEncontrado});
-            }catch(error){
-                error.statusCode = error.statusCode || 500;
-                res.status(error.statusCode).json({error: error.message});
-            }
-        }
+    try {
+      if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({ message: "ID do produto inválido" });
+      }
 
+      // Vverificar todos os campos do produto
+      const { name, description, images, available, state, owner, reciever } =
+        productData;
 
+      if (
+        !name ||
+        !description ||
+        !Array.isArray(images) ||
+        images.length === 0 ||
+        typeof available !== "boolean"
+      ) {
+        return res
+          .status(400)
+          .json({
+            message:
+              "Todos os campos obrigatórios devem ser preenchidos corretamente",
+          });
+      }
 
-        static async showUserProducts(req, res){
-            try{
-              const user = await UserService.getUserByToken(req);
-              const produtoEncontrado = await ProductService.showById(user.id);
-                res.json({produtoEncontrado});
-            }catch(error){
-                error.statusCode = error.statusCode || 500;
-                res.status(error.statusCode).json({error: error.message});
-            }
-        }
+      if (state && !["good", "fair", "bad"].includes(state)) {
+        return res.status(400).json({ message: "Estado do produto inválido" });
+      }
 
+      if (owner && !mongoose.Types.ObjectId.isValid(owner)) {
+        return res.status(400).json({ message: "ID do proprietário inválido" });
+      }
 
-        static async schedule(req, res){
-            try{
-                res.json({message:"schedule"});
-            }catch(error){
-                error.statusCode = error.statusCode || 500;
-                res.status(error.statusCode).json({error: error.message});
-            }
-        }
+      if (reciever && !mongoose.Types.ObjectId.isValid(reciever)) {
+        return res.status(400).json({ message: "ID do receptor inválido" });
+      }
 
+      const updatedProduct = await ProductService.updateProduct(
+        id,
+        productData
+      );
+
+      if (!updatedProduct) {
+        return res.status(404).json({ message: "Produto não encontrado" });
+      }
+
+      // Retornar o produto atualizado
+      res.status(200).json({ updatedProduct });
+    } catch (error) {
+      error.statusCode = error.statusCode || 500;
+      res.status(error.statusCode).json({ error: error.message });
+    }
+  }
+
+  static async show(req, res) {
+    const { id } = req.body;
+    try {
+      const produtoEncontrado = await ProductService.showById(id);
+      res.status(200).json({ produtoEncontrado });
+    } catch (error) {
+      error.statusCode = error.statusCode || 500;
+      res.status(error.statusCode).json({ error: error.message });
+    }
+  }
+
+  static async showUserProducts(req, res) {
+    try {
+      const user = await UserService.getUserByToken(req);
+      const produtoEncontrado = await ProductService.showById(user.id);
+      res.json({ produtoEncontrado });
+    } catch (error) {
+      error.statusCode = error.statusCode || 500;
+      res.status(error.statusCode).json({ error: error.message });
+    }
+  }
 
   static async concludeDonation(req, res) {
     try {
--- src/service/ProductService.js
+++ src/service/ProductService.js
@@ -1,9 +1,7 @@
-
 import mongoose from "mongoose";
-import Product from "../model/Product.js"
+import Product from "../model/Product.js";
 import UserService from "./UserService.js";
 import User from "../model/User.js";
-
 
 export default class ProductService {
   // Define o método estático assíncrono 'create' com os parâmetros 'req', 'name', 'description', 'state' e 'purchased_at'
@@ -52,191 +50,8 @@ export default class ProductService {
       error.statusCode = 422;
       // Lança o erro, interrompendo a execução do código
       throw error;
-
-export default class ProductService{
-    // Define o método estático assíncrono 'create' com os parâmetros 'req', 'name', 'description', 'state' e 'purchased_at'
-     static async create(req, name, description, state, purchased_at) {
-        // Obtém o usuário atual através do serviço de usuários usando as informações da requisição
-        const user = await UserService.getUser(req);
-
-        // Inicializa um array vazio para armazenar as imagens
-        let images = [];
-        // Verifica se há arquivos na requisição
-        if (req.files) {
-            // Se houver, armazena-os no array 'images'
-            images = req.files;
-        }
-
-        // Define a variável 'available' como true para indicar que o produto está disponível
-        const available = true;
-
-        // Verifica se o nome do produto foi fornecido
-        if (!name) {
-            // Se não, cria um novo erro com a mensagem "O nome é obrigatório."
-            const error = new Error("O nome é obrigatório.");
-            // Define o status code do erro para 422 (Unprocessable Entity)
-            error.statusCode = 422;
-            // Lança o erro, interrompendo a execução do código
-            throw error;
-        }
-
-        // Verifica se a descrição do produto foi fornecida
-        if (!description) {
-            // Se não, cria um novo erro com a mensagem "A descrição é obrigatória."
-            const error = new Error("A descrição é obrigatória.");
-            // Define o status code do erro para 422 (Unprocessable Entity)
-            error.statusCode = 422;
-            // Lança o erro, interrompendo a execução do código
-            throw error;
-        }
-
-        // Verifica se o estado do produto foi fornecido
-        if (!state) {
-            // Se não, cria um novo erro com a mensagem "O estado é obrigatório."
-            const error = new Error("O estado é obrigatório.");
-            // Define o status code do erro para 422 (Unprocessable Entity)
-            error.statusCode = 422;
-            // Lança o erro, interrompendo a execução do código
-            throw error;
-        }
-
-        // Verifica se a data de compra do produto foi fornecida
-        if (!purchased_at) {
-            // Se não, cria um novo erro com a mensagem "A data de compra é obrigatória."
-            const error = new Error("A data de compra é obrigatória.");
-            // Define o status code do erro para 422 (Unprocessable Entity)
-            error.statusCode = 422;
-            // Lança o erro, interrompendo a execução do código
-            throw error;
-        }
-
-        // Verifica se pelo menos uma imagem foi fornecida
-        if (images.length === 0) {
-            // Se não, cria um novo erro com a mensagem "A imagem é obrigatória."
-            const error = new Error("A imagem é obrigatória.");
-            // Define o status code do erro para 422 (Unprocessable Entity)
-            error.statusCode = 422;
-            // Lança o erro, interrompendo a execução do código
-            throw error;
-        }
-
-        // Cria uma nova instância de Product com os dados fornecidos, incluindo o ID do proprietário (user._id) e um array vazio de imagens
-        const product = new Product({name, description, state, owner: user._id, available, images: []});
-
-        // Itera sobre o array de imagens e adiciona cada filename ao array de imagens do produto
-        images.map((image) => product.images.push(image.filename));
-
-        // Salva a nova instância de produto no banco de dados e aguarda a conclusão do processo
-        const productSaved = await product.save();
-
-        // Retorna o produto salvo como resultado da função
-        return productSaved;
     }
-
-    static async findByReciever(recieverId) {
-        try {
-            const products = await Product.find({ reciever: recieverId });
-            return products;
-        } catch (error) {
-            throw new Error('Erro ao buscar produtos pelo reciever');
-        }
-    }
-
-    static async updateProduct(id, productData) {
-        try {
-            const updatedProduct = await Product.findByIdAndUpdate(id, productData, { new: true });
-            return updatedProduct;
-        } catch (error) {
-            throw new Error('Erro ao atualizar produto');
-        }
-    }
-
-    // Define o método estático assíncrono 'index' com os parâmetros 'page' e 'limit'
-    static async index(page, limit) {
-        // Busca todos os produtos no banco de dados, ordenados pela data de criação em ordem decrescente
-        // Limita o número de produtos retornados ao valor de 'limit'
-        // Pula um número de produtos calculado pela fórmula '(page-1) * limit' para paginação
-        // Popula o campo 'owner' do produto, excluindo o campo 'password'
-        // Popula também o campo 'reciever' do produto
-        const products = await Product.find()
-            .sort("-createdAt")
-            .limit(limit)
-            .skip((page - 1) * limit)
-            .populate({ path: "owner", select: "-password" })
-            .populate("reciever");
-
-        // Retorna a lista de produtos encontrados
-        return products;
-    }
-
-    static async showById(id){
-      const iDFromReq = ProductService.productbyIdValidator(id);
-      const product = await ProductService.getProductById(iDFromReq);
-    return product
-    }
-
-     static async getUserByToken(req){
-        if(!req.user){
-            const error = new Error("Acesso Negado.");
-            error.statusCode = 401;
-            throw error;
-        }
-
-        const user = User.findOne({_id: req.user.id}).select("-password");
-        return user;
-
-    }
-
-    // Verifica se o estado do produto foi fornecido
-    if (!state) {
-      // Se não, cria um novo erro com a mensagem "O estado é obrigatório."
-      const error = new Error("O estado é obrigatório.");
-      // Define o status code do erro para 422 (Unprocessable Entity)
-      error.statusCode = 422;
-      // Lança o erro, interrompendo a execução do código
-      throw error;
-    }
-
-    // Verifica se a data de compra do produto foi fornecida
-    if (!purchased_at) {
-      // Se não, cria um novo erro com a mensagem "A data de compra é obrigatória."
-      const error = new Error("A data de compra é obrigatória.");
-      // Define o status code do erro para 422 (Unprocessable Entity)
-      error.statusCode = 422;
-      // Lança o erro, interrompendo a execução do código
-      throw error;
-    }
-
-    // Verifica se pelo menos uma imagem foi fornecida
-    if (images.length === 0) {
-      // Se não, cria um novo erro com a mensagem "A imagem é obrigatória."
-      const error = new Error("A imagem é obrigatória.");
-      // Define o status code do erro para 422 (Unprocessable Entity)
-      error.statusCode = 422;
-      // Lança o erro, interrompendo a execução do código
-      throw error;
-    }
-
-    // Cria uma nova instância de Product com os dados fornecidos, incluindo o ID do proprietário (user._id) e um array vazio de imagens
-    const product = new Product({
-      name,
-      description,
-      state,
-      owner: user._id,
-      available,
-      images: [],
-    });
-
-    // Itera sobre o array de imagens e adiciona cada filename ao array de imagens do produto
-    images.map((image) => product.images.push(image.filename));
-
-    // Salva a nova instância de produto no banco de dados e aguarda a conclusão do processo
-    const productSaved = await product.save();
-
-    // Retorna o produto salvo como resultado da função
-    return productSaved;
   }
-
   // Define o método estático assíncrono 'index' com os parâmetros 'page' e 'limit'
   static async index(page, limit) {
     // Busca todos os produtos no banco de dados, ordenados pela data de criação em ordem decrescente
@@ -254,10 +69,6 @@ export default class ProductService{
     // Retorna a lista de produtos encontrados
     return products;
   }
-
-  static async show() {}
-
-  static async update() {}
 
   static async deleteProduct() {
     //Verifica se o ID é válido
@@ -312,7 +123,6 @@ export default class ProductService{
       throw error;
     }
 
-
     // Obtém o usuário pelo token
     const user = await UserService.getUser(req);
 
@@ -332,47 +142,44 @@ export default class ProductService{
     return `A visita foi agendada com Sucesso, entre em contato com ${user.name}, pelo telefone, ${user.phone}`;
   }
 
-  static async concludeDonation() {}
-
   static idValidation(idFromReq, id) {
     if (idFromReq !== id) {
       const error = new Error("Id não encontrado");
       error.statusCode = 404;
       throw error;
+    }
+  }
 
-    static async productbyIdValidator(id){
-        if(!mongoose.Types.ObjectId.isValid(id)){
-            const error = new Error("Id da requisição não encontrado.");
-            error.statusCode = 404;
-            throw error;
-        }else{
-            return id; 
-        }
+  static async productbyIdValidator(id) {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      const error = new Error("Id da requisição não encontrado.");
+      error.statusCode = 404;
+      throw error;
+    } else {
+      return id;
+    }
+  }
+
+  static async getProductById(id) {
+    const product = await Product.findOne({ _id: id });
+    if (!product) {
+      const error = new Error("Produto não encontrado.");
+      error.statusCode = 404;
+      throw error;
+    } else {
+      return product;
+    }
+  }
+
+  static async getUserByToken(req) {
+    if (!req.user) {
+      const error = new Error("Acesso Negado.");
+      error.statusCode = 401;
+      throw error;
     }
 
-    static async getProductById(id){
-        const product = await Product.findOne({_id: id});
-             if(!product){
-                const error = new Error("Produto não encontrado.");
-                error.statusCode = 404;
-             throw error;
-        }
-        else{
-             return product;
-        }
-    }
-
-      static async getUserByToken(req){
-        if(!req.user){
-            const error = new Error("Acesso Negado.");
-            error.statusCode = 401;
-            throw error;
-        }
-
-        const user = User.findOne({_id: req.user.id}).select("-password");
-        return user;
-
-    }
+    const user = User.findOne({ _id: req.user.id }).select("-password");
+    return user;
   }
 
   static async getProductbyId(req) {
